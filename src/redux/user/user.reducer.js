c0071ab7973a485a7395a139410ea8185b5fe242
Adam Kurczynski <adkurczynskis@interia.pl>
2020-06-01 12:07:46 +0200

    Created cart selector

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,3 +1,5 @@
+import { setCurrentUser } from './user.types';
+
 
 const INITIAL_STATE = {
     currentUser: null
@@ -5,7 +7,7 @@ const INITIAL_STATE = {
 
 const userReducer = (state = INITIAL_STATE, action) => {
     switch(action.type) {
-        case 'SET_CURRENT_USER' :
+        case setCurrentUser.SET_CURRENT_USER:
             return {
                 ...state,
                 currentUser: action.payload
